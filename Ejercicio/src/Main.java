17fe14e040700f229abdcc5c934333e66698ee50
Kevinr3011 <121906672+Kevinr3011@users.noreply.github.com>
2023-01-16 23:34:03 +0100

    Add files via upload

---
+++ Ejercicio/src/Main.java
@@ -0,0 +1,25 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        suma(20,  10,  5 );
+
+    }
+
+    public static void suma( int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+
+        System.out.println(resultado);
+
+    }
+
+
+
+
+
+
+
+}
